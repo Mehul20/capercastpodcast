91d7c0ce8d85e12c79f42cccc2225a366c3c97e6
Mehul <mehulrastogi2002@gmail.com>
2021-08-30 22:32:33 -0400

    First changes

---
+++ src/maintext.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MainHead(){
+    return <div className="containerHead">
+        <div className="typeHead">
+            <span className="typewriter">
+        We are thinkers. We are dreamers.<br />
+        We are inventors. We are listeners.<br />
+        But most importantly...<br />
+        We are storytellers.<br />
+        Welcome to Capercast! 
+        </span>
+
+    </div>
+    </div>
+
+}
+export default MainHead;
